f16a23b6d192a670ae6e25a8e34d39e0563bd3e4
Jade Lerch <jale8578@colorado.edu>
2025-12-01 23:09:11 +0000

    finished main and sample data

---
+++ main.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Graph.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: ./program <inputfile> <size>" << endl;
+        return 1;
+    }
+
+    string filename = argv[1];
+    int size = stoi(argv[2]);
+
+    Graph map(size);
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error opening file." << endl;
+        return 1;
+    }
+
+    string line;
+
+    getline(file, line);
+
+    while (getline(file, line)) {
+        if (line.rfind("Edges", 0) == 0) break;
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        int x, y, h;
+        char comma;
+
+        ss >> x >> comma >> y >> comma >> h;
+        map.createPoint(x, y, h);
+    }
+
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        int x1, y1, x2, y2;
+        string type;
+        char comma;
+
+        ss >> x1 >> comma >> y1 >> comma >> x2 >> comma >> y2 >> comma >> type;
+        map.createPath(x1, y1, x2, y2, type);
+    }
+
+    file.close();
+
+    bool active = true;
+    int option;
+
+    while (active) {
+        cout << "\nSelect an option:\n"
+             << "1. Create point\n"
+             << "2. Create path\n"
+             << "3. Change height\n"
+             << "4. Change path\n"
+             << "5. Flatten point\n"
+             << "6. Flatten graph\n"
+             << "7. Get shortest path\n"
+             << "8. Show graph\n"
+             << "9. Show edges\n"
+             << "10. Exit program\n";
+
+        if (!(cin >> option)) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid input.\n";
+            continue;
+        }
+        int x, y, x1, y1, x2, y2, height;
+        string type;
+
+        switch (option) {
+            case 1:
+                cout << "Enter x y height: ";
+                cin >> x >> y >> height;
+                if (x >= size || x < 0 ||y >= size || y < 0 || height < 0) {
+                    cout << "Invalid data" << endl;
+                    break;
+                }
+                map.createPoint(x, y, height);
+                break;
+
+            case 2:
+                cout << "Enter x1 y1 x2 y2 type: ";
+                cin >> x1 >> y1 >> x2 >> y2 >> type;
+                if (x1 >= size || x1 < 0 || y1 >= size || y1 < 0 || x2 >= size || x2 < 0 || y2 >= size || y2 < 0) {
+                    cout << "Invalid data" << endl;
+                    break;
+                }
+                map.createPath(x1, y1, x2, y2, type);
+                break;
+
+            case 3:
+                cout << "Enter x y newHeight: ";
+                cin >> x >> y >> height;
+                if (x >= size || x < 0 ||y >= size || y < 0 || height < 0) {
+                    cout << "Invalid data" << endl;
+                    break;
+                }
+                map.setHeight(x, y, height);
+                break;
+
+            case 4:
+                cout << "Enter x1 y1 x2 y2 newType: ";
+                cin >> x1 >> y1 >> x2 >> y2 >> type;
+                if (x1 >= size || x1 < 0 || y1 >= size || y1 < 0 || x2 >= size || x2 < 0 || y2 >= size || y2 < 0) {
+                    cout << "Invalid data" << endl;
+                    break;
+                }
+                map.setPath(x1, y1, x2, y2, type);
+                break;
+
+            case 5:
+                cout << "Enter x y: ";
+                cin >> x >> y;
+                if (x >= size || x < 0 ||y >= size || y < 0) {
+                    cout << "Invalid data" << endl;
+                    break;
+                }
+                map.flatten(x, y);
+                break;
+
+            case 6:
+                map.flattenAll();
+                break;
+
+            case 7:
+                cout << "Enter x1 y1 x2 y2: ";
+                cin >> x1 >> y1 >> x2 >> y2;
+                if (x1 >= size || x1 < 0 || y1 >= size || y1 < 0 || x2 >= size || x2 < 0 || y2 >= size || y2 < 0) {
+                    cout << "Invalid data" << endl;
+                    break;
+                }
+                map.getBestPath(x1, y1, x2, y2);
+                break;
+
+            case 8:
+                map.displayMatrix();
+                break;
+
+            case 9:
+                map.displayPaths();
+                break;
+
+            case 10:
+                cout << "Exiting program.\n";
+                active = false;
+                break;
+
+            default:
+                cout << "Invalid option.\n";
+        }
+    }
+
+    return 0;
+}
